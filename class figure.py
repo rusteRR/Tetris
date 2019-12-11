b04ad6614071e68c12bd8f405e2eaeb368bc68d9
Artem Batygin <aa.batygin@gmail.com>
2019-12-11 19:13:04 +0300

    rotation

--- class figure.py
+++ class figure.py
@@ -3,7 +3,6 @@ from random import choice
 
 stack_height_cells = 24
 stack_width_cells = 12
-
 cell_size = 20
 stack_width_px = stack_width_cells * cell_size
 stack_height_px = stack_height_cells * cell_size
@@ -28,7 +27,10 @@ fps = 60
 class Figure:
     def __init__(self, form):
         self.points = list()
+        self.orn = 1
         self.tr = 1
+        self.r = 0
+        self.form = form
         if form == 'I':
             for i in range(4):
                 self.points.append([0, i + 4])
@@ -58,9 +60,6 @@ class Figure:
             for i in range(2):
                 self.points.append([1, i + 4])
 
-    def get_coords(self):
-        return self.points
-
     def falling(self):
         global pole, trg
         tr = 1
@@ -75,49 +74,340 @@ class Figure:
             y = self.points[len(self.points) - i - 1][1]
             if tr:
                 pole[x][y] = 0
-                pole[x + 1][y] = 1
                 self.points[len(self.points) - i - 1] = [x + 1, y]
             else:
                 pole[x][y] = 2
                 self.tr = 0
                 trg = 1
 
+        for i in range(len(self.points)):
+            x = self.points[len(self.points) - i - 1][0]
+            y = self.points[len(self.points) - i - 1][1]
+            if tr:
+                pole[x][y] = 1
+
     def left(self):
         global pole
         tr = 1
         for i in range(len(self.points)):
-            x = self.points[i][0]
-            y = self.points[i][1]
+            x = self.points[len(self.points) - i - 1][0]
+            y = self.points[len(self.points) - i - 1][1]
             point = pole[x][y - 1]
             if point != 1 and point != 0:
                 tr = 0
-        if tr:
-            for i in range(len(self.points)):
-                x = self.points[i][0]
-                y = self.points[i][1]
+        for i in range(len(self.points)):
+            x = self.points[len(self.points) - i - 1][0]
+            y = self.points[len(self.points) - i - 1][1]
+            if tr:
                 pole[x][y] = 0
-                pole[x][y - 1] = 1
-                self.points[i] = [x, y - 1]
+                self.points[len(self.points) - i - 1] = [x, y - 1]
+
+        for i in range(len(self.points)):
+            x = self.points[len(self.points) - i - 1][0]
+            y = self.points[len(self.points) - i - 1][1]
+            if tr:
+                pole[x][y] = 1
 
     def right(self):
         global pole
         tr = 1
         for i in range(len(self.points)):
-            x = self.points[i][0]
-            y = self.points[i][1]
+            x = self.points[len(self.points) - i - 1][0]
+            y = self.points[len(self.points) - i - 1][1]
             point = pole[x][y + 1]
             if point != 1 and point != 0:
                 tr = 0
-        if tr:
-            for i in range(len(self.points)):
-                x = self.points[len(self.points) - i - 1][0]
-                y = self.points[len(self.points) - i - 1][1]
+        for i in range(len(self.points)):
+            x = self.points[len(self.points) - i - 1][0]
+            y = self.points[len(self.points) - i - 1][1]
+            if tr:
                 pole[x][y] = 0
-                pole[x][y + 1] = 1
                 self.points[len(self.points) - i - 1] = [x, y + 1]
+
+        for i in range(len(self.points)):
+            x = self.points[len(self.points) - i - 1][0]
+            y = self.points[len(self.points) - i - 1][1]
+            if tr:
+                pole[x][y] = 1
 
     def rotate(self):
         global pole
+        if self.form == 'I':
+            x, y = self.points[1]
+            if self.orn == 1:
+                if (pole[x - 1][y] + pole[x - 2][y] + pole[x + 1][y]) == 0:
+                    self.orn = 2
+                    self.points[0] = [x - 1, y]
+                    self.points[2] = [x - 2, y]
+                    self.points[3] = [x + 1, y]
+                    pole[x][y + 1] = 0
+                    pole[x][y + 2] = 0
+                    pole[x][y - 1] = 0
+
+                    pole[x - 1][y] = 1
+                    pole[x - 2][y] = 1
+                    pole[x + 1][y] = 1
+            else:
+                if (pole[x][y + 1] + pole[x][y + 2] + pole[x][y - 1]) == 0:
+                    self.orn = 1
+                    self.points[0] = [x, y + 1]
+                    self.points[2] = [x, y + 2]
+                    self.points[3] = [x, y - 1]
+                    pole[x - 1][y] = 0
+                    pole[x - 2][y] = 0
+                    pole[x + 1][y] = 0
+
+                    pole[x][y + 1] = 1
+                    pole[x][y + 2] = 1
+                    pole[x][y - 1] = 1
+
+        elif self.form == "T":
+            x, y = self.points[1]
+            if self.orn == 1:
+                if pole[x - 1][y] == 0:
+                    self.orn = 2
+                    self.points[((self.r % 3) + 2) % 4] = [x - 1, y]
+                    pole[x - 1][y] = 1
+                    pole[x][y + 1] = 0
+                    self.r += 1
+
+            elif self.orn == 2:
+                if pole[x][y + 1] == 0:
+                    self.orn = 3
+                    self.points[((self.r % 3) + 2) % 4] = [x, y + 1]
+                    pole[x][y + 1] = 1
+                    pole[x + 1][y] = 0
+                    self.r += 1
+
+            elif self.orn == 3:
+                if pole[x + 1][y] == 0:
+                    self.orn = 4
+                    self.points[((self.r % 3) + 2) % 4] = [x + 1, y]
+                    pole[x + 1][y] = 1
+                    pole[x][y - 1] = 0
+                    self.r += 1
+
+            elif self.orn == 4:
+                if pole[x][y - 1] == 0:
+                    self.orn = 1
+                    self.points[((self.r % 3) + 2) % 4] = [x, y - 1]
+                    pole[x][y - 1] = 1
+                    pole[x - 1][y] = 0
+                    self.r += 1
+
+        elif self.form == 'J':
+            x, y = self.points[2]
+            if self.orn == 1:
+                if (pole[x - 1][y] + pole[x - 1][y + 1] + pole[x + 1][y]) == 0:
+                    self.orn = 2
+                    self.points[0] = [x - 1, y + 1]
+                    self.points[1] = [x - 1, y]
+                    self.points[3] = [x + 1, y]
+                    pole[x - 1][y - 1] = 0
+                    pole[x][y - 1] = 0
+                    pole[x][y + 1] = 0
+
+                    pole[x - 1][y + 1] = 1
+                    pole[x - 1][y] = 1
+                    pole[x + 1][y] = 1
+            elif self.orn == 2:
+                if (pole[x][y + 1] + pole[x][y - 1] + pole[x + 1][y + 1]) == 0:
+                    self.orn = 3
+                    self.points[0] = [x + 1, y + 1]
+                    self.points[1] = [x, y + 1]
+                    self.points[3] = [x, y - 1]
+                    pole[x - 1][y] = 0
+                    pole[x - 1][y + 1] = 0
+                    pole[x + 1][y] = 0
+
+                    pole[x][y + 1] = 1
+                    pole[x + 1][y + 1] = 1
+                    pole[x][y - 1] = 1
+
+            elif self.orn == 3:
+                if (pole[x + 1][y - 1] + pole[x + 1][y] + pole[x - 1][y]) == 0:
+                    self.orn = 4
+                    self.points[0] = [x + 1, y - 1]
+                    self.points[1] = [x + 1, y]
+                    self.points[3] = [x - 1, y]
+                    pole[x][y - 1] = 0
+                    pole[x + 1][y + 1] = 0
+                    pole[x][y + 1] = 0
+
+                    pole[x + 1][y - 1] = 1
+                    pole[x + 1][y] = 1
+                    pole[x - 1][y] = 1
+
+            elif self.orn == 4:
+                if (pole[x][y + 1] + pole[x][y - 1] + pole[x - 1][y - 1]) == 0:
+                    self.orn = 1
+                    self.points[0] = [x - 1, y - 1]
+                    self.points[1] = [x, y - 1]
+                    self.points[3] = [x, y + 1]
+                    pole[x - 1][y] = 0
+                    pole[x + 1][y] = 0
+                    pole[x + 1][y - 1] = 0
+
+                    pole[x - 1][y - 1] = 1
+                    pole[x][y - 1] = 1
+                    pole[x][y + 1] = 1
+
+        elif self.form == 'L':
+            x, y = self.points[2]
+            if self.orn == 1:
+                if (pole[x - 1][y] + pole[x + 1][y + 1] + pole[x + 1][y]) == 0:
+                    self.orn = 2
+                    self.points[0] = [x + 1, y + 1]
+                    self.points[1] = [x - 1, y]
+                    self.points[3] = [x + 1, y]
+                    pole[x - 1][y + 1] = 0
+                    pole[x][y - 1] = 0
+                    pole[x][y + 1] = 0
+
+                    pole[x + 1][y + 1] = 1
+                    pole[x - 1][y] = 1
+                    pole[x + 1][y] = 1
+            elif self.orn == 2:
+                if (pole[x][y + 1] + pole[x][y - 1] + pole[x + 1][y - 1]) == 0:
+                    self.orn = 3
+                    self.points[0] = [x + 1, y - 1]
+                    self.points[1] = [x, y + 1]
+                    self.points[3] = [x, y - 1]
+                    pole[x - 1][y] = 0
+                    pole[x + 1][y + 1] = 0
+                    pole[x + 1][y] = 0
+
+                    pole[x][y + 1] = 1
+                    pole[x + 1][y - 1] = 1
+                    pole[x][y - 1] = 1
+
+            elif self.orn == 3:
+                if (pole[x - 1][y - 1] + pole[x + 1][y] + pole[x - 1][y]) == 0:
+                    self.orn = 4
+                    self.points[0] = [x - 1, y - 1]
+                    self.points[1] = [x + 1, y]
+                    self.points[3] = [x - 1, y]
+                    pole[x][y - 1] = 0
+                    pole[x + 1][y - 1] = 0
+                    pole[x][y + 1] = 0
+
+                    pole[x - 1][y - 1] = 1
+                    pole[x + 1][y] = 1
+                    pole[x - 1][y] = 1
+
+            elif self.orn == 4:
+                if (pole[x][y + 1] + pole[x][y - 1] + pole[x - 1][y + 1]) == 0:
+                    self.orn = 1
+                    self.points[0] = [x - 1, y + 1]
+                    self.points[1] = [x, y - 1]
+                    self.points[3] = [x, y + 1]
+                    pole[x - 1][y] = 0
+                    pole[x + 1][y] = 0
+                    pole[x - 1][y - 1] = 0
+
+                    pole[x - 1][y + 1] = 1
+                    pole[x][y - 1] = 1
+                    pole[x][y + 1] = 1
+
+        elif self.form == 'S':
+            x, y = self.points[3]
+            if self.orn == 1:
+                if (pole[x + 1][y + 1] + pole[x][y + 1]) == 0:
+                    self.orn = 2
+                    self.points[0] = [x, y + 1]
+                    self.points[1] = [x + 1, y + 1]
+                    self.points[2] = [x - 1, y]
+                    pole[x - 1][y + 1] = 0
+                    pole[x][y - 1] = 0
+
+                    pole[x + 1][y + 1] = 1
+                    pole[x][y + 1] = 1
+
+            elif self.orn == 2:
+                if (pole[x + 1][y] + pole[x + 1][y - 1]) == 0:
+                    self.orn = 3
+                    self.points[0] = [x + 1, y]
+                    self.points[1] = [x + 1, y - 1]
+                    self.points[2] = [x, y + 1]
+                    pole[x + 1][y + 1] = 0
+                    pole[x - 1][y] = 0
+
+                    pole[x + 1][y - 1] = 1
+                    pole[x + 1][y] = 1
+
+            elif self.orn == 3:
+                if (pole[x - 1][y - 1] + pole[x][y - 1]) == 0:
+                    self.orn = 4
+                    self.points[0] = [x, y - 1]
+                    self.points[1] = [x - 1, y - 1]
+                    self.points[2] = [x + 1, y]
+                    pole[x + 1][y - 1] = 0
+                    pole[x][y + 1] = 0
+
+                    pole[x][y - 1] = 1
+                    pole[x - 1][y - 1] = 1
+
+            elif self.orn == 4:
+                if (pole[x - 1][y + 1] + pole[x - 1][y]) == 0:
+                    self.orn = 1
+                    self.points[0] = [x - 1, y]
+                    self.points[1] = [x - 1, y + 1]
+                    self.points[2] = [x, y - 1]
+                    pole[x - 1][y - 1] = 0
+                    pole[x + 1][y] = 0
+
+                    pole[x - 1][y + 1] = 1
+                    pole[x - 1][y] = 1
+
+        elif self.form == 'Z':
+            x, y = self.points[2]
+            if self.orn == 1:
+                if (pole[x + 1][y] + pole[x - 1][y + 1]) == 0:
+                    self.orn = 2
+                    self.points[0] = [x - 1, y + 1]
+                    self.points[1] = [x, y + 1]
+                    self.points[3] = [x + 1, y]
+                    pole[x - 1][y] = 0
+                    pole[x - 1][y - 1] = 0
+
+                    pole[x - 1][y + 1] = 1
+                    pole[x + 1][y] = 1
+
+            elif self.orn == 2:
+                if (pole[x + 1][y + 1] + pole[x][y - 1]) == 0:
+                    self.orn = 3
+                    self.points[0] = [x + 1, y + 1]
+                    self.points[1] = [x + 1, y]
+                    self.points[3] = [x, y - 1]
+                    pole[x][y + 1] = 0
+                    pole[x - 1][y + 1] = 0
+
+                    pole[x + 1][y + 1] = 1
+                    pole[x][y - 1] = 1
+
+            elif self.orn == 3:
+                if (pole[x - 1][y] + pole[x + 1][y - 1]) == 0:
+                    self.orn = 4
+                    self.points[0] = [x + 1, y - 1]
+                    self.points[1] = [x, y - 1]
+                    self.points[3] = [x - 1, y]
+                    pole[x + 1][y] = 0
+                    pole[x + 1][y + 1] = 0
+
+                    pole[x + 1][y - 1] = 1
+                    pole[x - 1][y] = 1
+
+            elif self.orn == 4:
+                if (pole[x - 1][y - 1] + pole[x][y + 1]) == 0:
+                    self.orn = 1
+                    self.points[0] = [x - 1, y - 1]
+                    self.points[1] = [x - 1, y]
+                    self.points[3] = [x, y + 1]
+                    pole[x][y - 1] = 0
+                    pole[x + 1][y - 1] = 0
+
+                    pole[x - 1][y - 1] = 1
+                    pole[x][y + 1] = 1
 
 
 def render():
@@ -139,12 +429,9 @@ def render():
 
 
 im = ('I', 'J', 'L', 'O', 'S', 'T', 'Z')
-
-
 a = 60
 run = True
 trg = 1
-c = 1
 while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -154,9 +441,11 @@ while run:
                 figure.left()
             if event.key == pygame.K_RIGHT:
                 figure.right()
+            if event.key == pygame.K_UP:
+                figure.rotate()
         if event.type == pygame.KEYUP or event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                a = 0
+                a = -1
     pygame.display.flip()
     render()
     if a < 0:
